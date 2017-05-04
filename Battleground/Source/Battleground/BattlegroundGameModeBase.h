a2fe3684c3d66f0a940955ad23be14e475c2ec3c
Eric Holt <freydenkt@gmail.com>
2017-05-03 21:24:07 -0400

    Lecture 101 move .gitignore file

---
+++ Battleground/Source/Battleground/BattlegroundGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "BattlegroundGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEGROUND_API ABattlegroundGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
