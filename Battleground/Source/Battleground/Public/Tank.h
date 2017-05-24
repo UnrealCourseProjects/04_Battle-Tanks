6bb54996b191b8653e0ecf44584cd11eda1c5555
Eric Holt <freydenkt@gmail.com>
2017-05-24 15:29:05 -0400

    Section 04 Lecture 191 - Using Tank Damage on Actors - figured out the Projectile bug - poorly placed socket on barrel

--- Battleground/Source/Battleground/Public/Tank.h
+++ Battleground/Source/Battleground/Public/Tank.h
@@ -10,7 +10,17 @@ class BATTLEGROUND_API ATank : public APawn
 {
 	GENERATED_BODY()
 
-private:	
+public:	
 	// Sets default values for this pawn's properties
 	ATank();
+
+	// called by the engine when actor damage is dealt.
+	virtual float TakeDamage (float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category ="Setup")
+	int32 StartingHealth = 100;
+
+	UPROPERTY(VisibleAnywhere, Category ="Setup")
+	int32 CurrentHealth = StartingHealth;
 };
